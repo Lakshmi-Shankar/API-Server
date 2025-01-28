ee57a1bb859d3f19060ac8f943a0ae6ce7cfb091
Lakshmi-Shankar <lakshmi7708671565@gmail.com>
2025-01-28 14:09:15 +0530

    Server Added

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const PORT = 4000;
+
+// app.get("/",(req,res)=>{
+//     res.send("Hi I am Lakshmi Shankar!");
+// })
+
+// app.listen(PORT, ()=>{
+//     console.log(`The HTTP link---> http://localhost:${PORT}`);
+// })
+
+const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 3100;
 
-app.get("/",(res,req)=>{
-    res.send("Hi I am Lakshmi Shankar!");
-})
+app.get('/', (req, res) => {
+    res.send('Hello, Kalvians! Your backend server is live!');
+});
 
-app.listen(PORT, ()=>{
-    console.log(`The HTTP link---> http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
